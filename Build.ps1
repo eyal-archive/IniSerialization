17a4fe568a92214017c808e10f52d779d9a3a46e
Eyal Shilony <esh@shilony.net>
2013-06-12 05:02:22 +0300

    Update the build file

--- Build.ps1
+++ Build.ps1
@@ -48,7 +48,7 @@ Task Clean {
 	}
 }
 
-# Copies the artifacts to the build directory
+# Copies artifacts to the build directory
 Task Artifacts {
     New-Item $artifactsPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
@@ -60,13 +60,13 @@ Task Artifacts {
     {
         foreach($artifact in $artifacts)
         {
-            $assemblyPath = "$($projInfo.Output)$artifact"
+            $assemblyPath = "$($projInfo.ProjectOutput)$artifact"
 
-            if ([System.IO.File]::Exists($assemblyPath) -and $artifactList.Contains($artifact))
+            if ([System.IO.File]::Exists($assemblyPath) -and $artifactList -contains $artifact)
 		    {
                 Write-Host "Copy file '$artifact' to '$artifactsPath'."
     
-                gci "$assemblyPath" | copy -Destination "$artifactsPath"
+                copy $assemblyPath "$artifactsPath"
                    
                 $artifactList.Remove($artifact)
             }
@@ -113,27 +113,11 @@ Task Nuget {
 Task Default-Build {
 	foreach($projInfo in Get-Projects-Info)
 	{
-		$projFile = $projInfo.File
+		$projFile = $projInfo.FullName
 		
 		try
 		{
-			exec { msbuild /nologo /v:$msbuildVerbosity /m:$msbuildCpuCount /p:BuildInParralel=$msbuildParralel /p:Configuration="$buildConfig" /p:Platform="AnyCPU" /p:OutDir="$($projInfo.Output)" "$projFile" }
-		}
-		catch
-		{
-            Exit-Build "Failed to build project '$projFile'"
-		}
-	}
-}
-
-Task Debug-Build -Depends Clean, Nuget {
-    foreach($projInfo in Get-Projects-Info)
-	{
-		$projFile = $projInfo.File
-		
-		try
-		{
-			exec { msbuild /nologo /v:$msbuildVerbosity /m:$msbuildCpuCount /p:BuildInParralel=$msbuildParralel /p:Configuration="Debug" /p:Platform="AnyCPU" /p:OutDir="$artifactsPath\" "$projFile" }
+			exec { msbuild /nologo /v:$msbuildVerbosity /m:$msbuildCpuCount /p:BuildInParralel=$msbuildParralel /p:Configuration="$buildConfig" /p:Platform="AnyCPU" /p:OutDir="$($projInfo.ProjectOutput)" "$projFile" }
 		}
 		catch
 		{
@@ -149,57 +133,44 @@ Task Debug-Build -Depends Clean, Nuget {
 Task Unit-Test {
 	$tools = @("xunit.console", "xunit.console.x86", "xunit.console.clr4", "xunit.console.clr4.x86")
 
-    $outputRegex = New-Object Regex -ArgumentList @("xUnit\.net console test runner.+?Could not load file or assembly.+?(?:cannot be loaded.|incorrect format.)")
+    $badOutputPattern = "xUnit\.net console test runner.+?Could not load file or assembly.+?(?:cannot be loaded.|incorrect format.)"
 
-    $failureRegex = New-Object Regex -ArgumentList @(",\s+[1-9]\d*\s+failed,")
+    $failurePattern = ",\s+[1-9]\d*\s+failed,"
 
-    $successRegex = New-Object Regex -ArgumentList @("Test assembly:.+seconds")
+    $successPattern = "Test assembly:.+seconds"
 
 	foreach($projInfo in Get-Projects-Info)
 	{
-		$projDocument = New-Object XML
-		
-		$projDocument.Load($projInfo.File)
-		
-        # Gets the assembly's name out of the project's file
-		$assemblyName = ($projDocument.Project.PropertyGroup | where { $_.AssemblyName -ne $null }).AssemblyName
-		
-		$assemblyPath = "$($projInfo.Output)$assemblyName.dll"
-		
-		if ([System.IO.File]::Exists($assemblyPath))
-		{
-            # Checks that xUnit is referenced so we can determine whether it's a project that may contain tests
-			$isTestProject = ($projDocument.Project.ItemGroup.Reference.Include | where { $_ -eq "xunit" }) -ne $null
+        if ($projInfo.IsTestProject)
+        {
+            $assemblyPath = "$($projInfo.ProjectOutput + $projInfo.AssemblyName).dll"
 
-			if ($isTestProject)
-			{
-                # Iterates over xUnit executables and attempt to find a candidate to execute the assembly
-				foreach($tool in $tools)
-				{
-					$output = & $(Get-Tool $tool) $assemblyPath /silent
+            # Iterates over xUnit executables in attempt to find the correct version of the tool to load the assembly
+		    foreach($tool in $tools)
+		    {
+			    $output = "$(& $(Get-Tool $tool) $assemblyPath /silent)"
 
-					if (-not $outputRegex.IsMatch($output))
-					{
-                        if ($failureRegex.IsMatch($output))
+			    if ($output -notmatch $badOutputPattern)
+			    {
+                    if ($output -match $failurePattern)
+                    {
+                        Exit-Build "One or more tests failed"
+                    }
+                    else
+                    {
+                        $matched = $output -match $successPattern
+
+                        if ($matched)
                         {
-                            Exit-Build "One or more tests failed"
+                            echo $($matches[0])
                         }
-                        else
-                        {
-                            $outputMatch = $successRegex.Match($output)
 
-                            if ($outputMatch.Success)
-                            {
-                                echo $outputMatch.Value
-                            }
-
-                            break
-                        }
-					}
-				}
-			}
-		}
-	}
+                        break
+                    }
+			    }
+		    }
+        }
+	}           
 }
 
 # Utility Functions
@@ -213,10 +184,7 @@ function Exit-Build
         [Parameter(Position = 0, Mandatory = $true)][String]$Message
     )
 
-    Write-Host
-	Write-Host $([String]::Format("Exiting build because task [{0}] failed.", $psake.context.Peek().currentTaskName)) -ForegroundColor Red
-    Write-Host "->    $Message." -ForegroundColor Red
-	Write-Host
+	Write-Host $("`nExiting build because task [{0}] failed.`n->`t$Message.`n" -f $psake.context.Peek().currentTaskName) -ForegroundColor Red
 
     Exit
 }
@@ -229,7 +197,9 @@ function Get-Tool
         [Parameter(Position = 0, Mandatory = $true)][String]$ToolFile
     )
 
-	return @(gci "$baseDir" -Recurse -File -Include "$ToolFile.exe") | select -First 1
+	$tool = (@(gci "$baseDir" -Recurse -File -Filter "$ToolFile.exe") | select -First 1)
+
+    return $tool.FullName
 }
 
 # Gets information about projects in the current directory of the solution
@@ -243,18 +213,34 @@ function Get-Projects-Info
 	$paths = New-Object System.Collections.Generic.List[PSObject]
 
     # Gets the project files based on a given file's extension
-    $projFiles = @(gci "$baseDir" -Recurse -File -Include "*.$ProjectExt")
+    $projFiles = @(gci "$baseDir" -Recurse -File -Filter "*.$ProjectExt")
 
 	foreach($file in $projFiles)
 	{
 		$projDir = $file.Directory
-		
-		$bin = "$projDir\bin\$buildConfig\"
-		
+
+        $fullName = $file.FullName
+
+        $bin = "$projDir\bin\$buildConfig\"
+
+        $projDocument = New-Object XML
+
+		$projDocument.Load($fullName)
+
+        # Checks that xUnit is referenced so we can determine whether it's a project that may contain tests
+		$isTestProject = ($projDocument.Project.ItemGroup.Reference.Include | where { $_ -eq "xunit" }) -ne $null
+
+        # Gets the assembly's name out of the project's file
+		$assemblyName = ($projDocument.Project.PropertyGroup | where { $_.AssemblyName -ne $null }).AssemblyName
+
 		$paths.Add(@{
 			File = $file;
 			Directory = $projDir;
-			Output = $bin
+            FullName = $fullName;
+            ProjectOutput = $bin;
+			ReleaseOutput = "$artifactsPath\";
+            AssemblyName = $assemblyName;
+            IsTestProject = $isTestProject
 		})
 	}
 	
